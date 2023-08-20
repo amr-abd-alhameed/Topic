dd5fc079f865ad473ffa66c2724a98017a148dbf
amr <aa5473@fayoum.edu.eg>
2023-08-20 18:19:54 +0300

    fixing some errors for the first time

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} transition-all duration-200`}>
+      <body className={inter.className}>
         <ThemeProvider>
           <Header />
           {children}
