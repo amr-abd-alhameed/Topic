14d538861421285f3c196b97594f1f1849af8925
amr-abd-alhameed <114324069+amr-abd-alhameed@users.noreply.github.com>
2023-08-21 12:20:45 +0300

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,6 +1,7 @@
-"use client";import React from "react";
+"use client";
+import React from "react";
 const Main = ({ props }) => {
-  return <main>{props.children}</main>;
+  return <main>{props.Children}</main>;
 };
 
 export default Main;
